6e085f8d08a0f83494adde97eb031abbd05c8590
andrewkkuo <99966485+andrewkkuo@users.noreply.github.com>
2022-06-01 19:54:11 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@
 #define CENTER_BASE 1500
 
 Ticker timer;
+int temp1 = 3;
+int temp3 = 3;
 int temp = 10;
 Ticker encoder_ticker;
 Ticker servo_ticker;
@@ -22,7 +24,7 @@ BBCar car(pin5, pin6, servo_ticker);
 DigitalInOut FR(D2), R(D3), L(D4), FL(D7);
 DigitalInOut pin10(D10);
 int first = 0;
-Thread s(osPriorityLow);
+Thread s(osPriorityBelowNormal);
 int nextt = 0;
 int nexttt = 0;
 float llast = 0;
@@ -38,7 +40,7 @@ erpc::SimpleServer rpc_server;
 BBCarService_service bbcar_service;
 
 void info() {
-    while(1) {
+    while(1){
         printf("distance: %f\n", steps *6.5 * 3.14 / 32);
         printf("current speed: %f\n", num);
         ThisThread::sleep_for(1s);
@@ -78,6 +80,7 @@ int main()
     steps = 0;
     last = 0;
     u.start(callfunc);
+    timer.attach(&calculate, 1s);
     s.start(rpc);
     while (1) {
         FR.output();
@@ -95,70 +98,84 @@ int main()
         L.input();
         wait_us(230);
         int pattern = FL * 1000 + L * 100 + R * 10 + FR;
-        // printf("%d\n",pattern);
-        if (ping1 < 15) {
+        if (ping1 < 20) {
+            ThisThread::sleep_for(200ms);
             car.turnaround();
-            ThisThread::sleep_for(2500ms);
+            ThisThread::sleep_for(1200ms);
             nextt = 2;
         }
         else if (pattern == 111) {
             car.goStraight(70);
-            nextt = 1;
             temp--;
+            if (temp <= 0) {
+                nextt = 1;
+            }
         }
         else if (pattern == 1111) {
-            if (nexttt == 1) {
-                car.turn(200, 0.00000001);
+            if (nextt == 1) {
+                car.turn(200, -0.00000001);
                 ThisThread::sleep_for(500ms);
             }
-            else if (nextt == 1 && temp <= 0) {
-                car.turn(200, -0.00000001);
-                ThisThread::sleep_for(600ms);
-                nextt = 0;
-            } 
             else if (nextt == 2) {
-                car.turn(200, 0.00000001);
+                car.turn(200, 0.1);
                 ThisThread::sleep_for(450ms);
-                nextt = 0;
-            } else car.goStraight(70);
+            } else if (nextt == 0) car.goStraight(70);
         }
         else if (pattern == 1110) {
-            if ((nextt == 0) && first == 0) {
+            if (first == 0 && temp3 <= 0) {
                 car.turnaround();
-                ThisThread::sleep_for(1650ms);
-                nexttt = 1;
+                ThisThread::sleep_for(1200ms);
+                nextt = 2;
                 first++;
             }
-            temp -= 1;
-            nextt = 2;
+            temp3--;
+            temp1--;
+            if (temp1 <= 0){
+                nextt = 2;
+            }
         }
         else if (pattern == 1000) {
             car.turn(200, 0.0000000001);
             pin5 = 0;
+            temp = 3;
+            temp1 = 3;
         }
         else if (pattern == 1100) {
             car.turn(100, 0.1);
+            temp = 3;
+            temp1 = 3;
         }
         else if (pattern == 100) {
             car.turn(160, 0.7);
+            temp = 3;
+            temp1 = 3;
         }
         else if (pattern == 110) {
             car.goStraight(70);
+            temp = 3;
+            temp1 = 3;
         }
         else if (pattern == 10) {
             car.turn(160, -0.5);
+            temp = 3;
+            temp1 = 3;
         }
         else if (pattern == 11) {
             car.turn(100, -0.1);
+            temp = 3;
+            temp1 = 3;
         }
         else if (pattern == 1) {
             car.turn(200, -0.00000001);
             pin6 = 0;
+            temp = 3;
+            temp1 = 3;
         } 
         else if (pattern == 0) {
             car.stop();
         }
         else car.goStraight(70);
+        ThisThread::sleep_for(20ms);
     }
     return 0;
 }
